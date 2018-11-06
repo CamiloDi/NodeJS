c0b267e9b4e7cef82df5c13c3b2913f7844efa2e
camilodi <camilodiazsev@gmail.com>
2018-11-05 22:46:32 -0300

    1.1

--- Curso NodeJS/03-bases-node/app.js
+++ Curso NodeJS/03-bases-node/app.js
@@ -1,29 +1,31 @@
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
+const argv= require('./config/yargs').argv;
+const colors = require('colors');
 
 const {
-    crearArchivo
+    crearArchivo,listarTabla
 } = require('./multiplicar/multiplicar')
 
+let comando = argv._[0];
+
+switch(comando){
+    case 'listar':
+    listarTabla(argv.base,argv.limite)
+                        .then(tabla=>console.log(tabla.red))
+                        .catch(err=>console.log(err));
+    break;
+
+    case 'crear':
+    crearArchivo(argv.base,argv.limite)
+    .then(archivo => console.log(`archivo creado ${colors.green(archivo)}`.red))
+   .catch(err => console.log(err));
+    break;
+
+    default:
+    console.log('comando no reconocido');
+}
+
+// console.log(argv.base);
 
 
-let argv2 = process.argv;
-
-// let param= argv[2];
-// let base =param.split('=')[1];
 
 
-crearArchivo(base)
-    .then(archivo => console.log(`archivo creado ${archivo}`))
-    .catch(err => console.log(err));
--- Curso NodeJS/03-bases-node/multiplicar/multiplicar.js
+++ Curso NodeJS/03-bases-node/multiplicar/multiplicar.js
@@ -4,15 +4,36 @@ const fs = require('fs');
 
 //module.exports.crearArchivo
 
-let crearArchivo = (base)=>{
+let listarTabla = (base, limite)=>{
+return new Promise((resolve,reject)=>{
+    if(!Number(base)){
+        reject(`El valor ingresado de la base: ${base}, no es un numero`);return;
+    }
+    if(!Number(limite)){
+        reject(`El valor ingresado del limite : ${base}, no es un numero`);return;
+    }
+    let data='';
 
+        for(let i=0;i<=limite;i++){
+         data+=`${base}*${i}=${base*i}\n`;
+            
+        }
+        resolve(`${data}`);
+});
+}
+
+
+let crearArchivo = (base,limite=10)=>{
     return new Promise((resolve,reject)=>{
         if(!Number(base)){
             reject(`El valor ingresado: ${base}, no es un numero`);return;
         }
+        if(!Number(limite)){
+            reject(`El valor ingresado del limite : ${base}, no es un numero`);return;
+        }
         let data='';
 
-        for(let i=0;i<11;i++){
+        for(let i=0;i<=limite;i++){
          data+=`${base}*${i}=${base*i}\n`;
             
         }
@@ -24,7 +45,7 @@ let crearArchivo = (base)=>{
 
     });
 }
-module.exports={crearArchivo}
+module.exports={crearArchivo,listarTabla}
 
 
 
