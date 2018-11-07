fee84ad30c14e6bd4b8e06b855beb23790013449
Camilo Díaz S <camilodiazsev@gmail.com>
2018-11-07 15:31:57 -0300

    1.01

--- SlackBOt/index.js
+++ SlackBOt/index.js
@@ -1,52 +1,101 @@
-'use strict';
-var SlackBot = require("slackbots");
-var fs = require('fs');
-var channel = "pruebasbot"
-var user="UCYTNCWR2";
-    var bot = new SlackBot({
-        token: "xoxb-441306192277-466081881904-fcO2YDRKnOFHO1DnLnjbkkwD",
-        name: "Mr. Bot"
-    });
-    bot.on("start", function() {
-      //bot.postMessageToChannel(channel, "Hello world!");
-      //console.log("Hello world!");
-  });
+const SlackBot = require("slackbots");
+const fs = require('fs');
+const datos = require('./data/datos.json');
 
-  bot.on("message", function(data) {
+let user = "";
+let listaUsuarios = [];
+
+var bot = new SlackBot({
+    token: "xoxb-441306192277-466081881904-fcO2YDRKnOFHO1DnLnjbkkwD",
+    name: "Mr. Test-Bot"
+});
+bot.on("start", function() {
+    let datosUsuarios = bot.getUsers();
+    listaUsuarios = datosUsuarios._value.members;
+    //console.log(listaUsuarios[0]);
+
+});
+
+bot.on("message", function(data) {
     if (data.type !== "message") {
-      return;
-  }
-  
-  handleMessage(data);
-  });
+        return;
+    }
+    let userID = data.user;
+    if (validaUsuario(userID)) {
+        responder(data.text.toLowerCase());
+    }
 
-  function handleMessage(data) {
-    //console.log(data);
-    switch(data.text) {
-        case "hi":
-            BuscarTexto();
-            break;
-        case 'RANCO':
-        bot.postMessageToUser(user,'Produccion');
-        break;
-        case 'RAPEL':
-        bot.postMessageToUser(user,'Desarrollo');
-        //bot.postMessage(data.user, JSON.stringify({'rapel':bot})); 
-        console.log(data);
-        break;
-        default:
-        //bot.postMessage(message.user, 'meow!', { 'pruebatron': true, icon_emoji: ':cat:' }); 
-        break;
+});
+
+function validaUsuario(userID) {
+    let index = listaUsuarios.findIndex(usuario => usuario.id === userID);
+
+    if (index >= 0) {
+        user = listaUsuarios[index].name;
+        return true;
+    } else if (userID != undefined) {
+        console.log(index, userID);
+        console.log('usuario no encontrado! :(');
+        return false;
     }
 }
-function BuscarTexto() {
-  var greeting = leerArchivo();
-  bot.postMessageToUser(user, greeting.lista.Cesantia);
+
+function responder(mensajeRecibido) {
+    let arregloMensaje = mensajeRecibido.split(' busca ');
+    let index = arregloMensaje.findIndex(texto => texto === 'vsbot');
+    //console.log(mensajeRecibido);
+    if (index >= 0) {
+        BuscarJson(arregloMensaje[index + 1]);
+    } else {
+        switch (mensajeRecibido) {
+            case 'hola':
+                bot.postMessageToUser(user, 'Hola!, en que te puedo ayudar? :grin:');
+                break;
+            case 'como estas?':
+                bot.postMessageToUser(user, 'Mejor que tu');
+                break;
+            default:
+                bot.postMessageToUser(user, 'No entendi lo que me pediste :thinking_face:');
+        }
+    }
 }
+
+function BuscarJson(criteroBusqueda) {
+    let objetoDatos = datos; //leerArchivo();
+    //bot.postMessageToUser(user, objetoDatos);
+    let mensaje = '';
+    switch (criteroBusqueda) {
+        case 'esquemas':
+            mensaje = '*Esquemas:* \n';
+            let totalEsq = objetoDatos.Esquemas.length;
+            for (let i = 0; i < totalEsq; i++) {
+                mensaje += `${objetoDatos.Esquemas[i].nom} | ${objetoDatos.Esquemas[i].desc}\n`
+            }
+            bot.postMessageToUser(user, mensaje);
+            break;
+        case 'servidores':
+            mensaje = '*Producción:* \n';
+            let totalServ = objetoDatos.Servidores.Produccion.length;
+            for (let i = 0; i < totalServ; i++) {
+                mensaje += `${objetoDatos.Servidores.Produccion[i].desc} | ${objetoDatos.Servidores.Produccion[i].ip}\n`
+            }
+            mensaje += '*QA:* \n';
+            totalServ = objetoDatos.Servidores.QA.length;
+            for (let i = 0; i < totalServ; i++) {
+                mensaje += `${objetoDatos.Servidores.QA[i].desc} | ${objetoDatos.Servidores.QA[i].ip}\n`
+            }
+            mensaje += '*Desarrollo:* \n';
+            totalServ = objetoDatos.Servidores.Desarrollo.length;
+            for (let i = 0; i < totalServ; i++) {
+                mensaje += `${objetoDatos.Servidores.Desarrollo[i].desc} | ${objetoDatos.Servidores.Desarrollo[i].ip}\n`
+            }
+            bot.postMessageToUser(user, mensaje)
+            break;
+        default:
+            bot.postMessageToUser(user, 'No pude encontrar lo que me pediste :disappointed:');
+    }
+}
+
 function leerArchivo() {
-
-  return JSON.parse(fs.readFileSync('C:\\Calculo.json', 'utf8'));
-
+    return JSON.parse(fs.readFile('./data/datos.json', 'utf8'));
 }
-
-
