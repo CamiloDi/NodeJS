0939e610257d5eaadbafb83807a92a536f8b022c
Camilo Díaz S <camilodiazsev@gmail.com>
2018-11-08 09:33:33 -0300

    1.1

--- Curso NodeJS/05-clima-mundo/clima/clima.js
+++ Curso NodeJS/05-clima-mundo/clima/clima.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
-const getClima = async (lat, lng) => {
+const getClima = async(lat, lng) => {
 
     let encodeLat = encodeURI(lat);
     let encodeLng = encodeURI(lng);
-    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${encodeLat}&lon=${encodeLng}&units=metric&appid=34409ed535e9e733e345b76bf4219031`);
-    
+    let key = 'PEGA-AQUI-TU-KEY';
+    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${encodeLat}&lon=${encodeLng}&units=metric&appid=${key}`);
+
     if (resp.data.cod !== 200) throw new Error(`Hubo un error al obtener el clima:  ${resp.message}`)
-    //console.log(resp.data.main);
+        //console.log(resp.data.main);
     let clima = resp.data.main.temp;
     return clima;
 }
