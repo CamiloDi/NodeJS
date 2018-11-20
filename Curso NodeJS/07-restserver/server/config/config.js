d7ac3cb8a24def1c2da1f0b566dcd154320718fb
camilodi <camilodiazsev@gmail.com>
2018-11-19 22:29:29 -0300

    1.5.1

--- Curso NodeJS/07-restserver/server/config/config.js
+++ Curso NodeJS/07-restserver/server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb://cafe-user:Warcraft.3@ds037824.mlab.com:37824/cafe';
+    urlDB=process.env.MONGO_URI;
 }
 process.env.URLDB=urlDB;
 
